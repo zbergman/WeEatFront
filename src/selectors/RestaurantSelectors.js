f68862ccebd2614aa4b8c83f8a8c8ebdfb417bdb
zohar.bergman <zohar.bergman@wework.com>
2019-09-15 10:17:51 +0300

    Make the restaurants list component to be pure component, create a selector that returns that filtered restaurants.

--- src/selectors/RestaurantSelectors.js
+++ src/selectors/RestaurantSelectors.js
@@ -2,8 +2,20 @@ import { createSelector } from "reselect";
 
 const getCurrentRestaurantById = state =>
   state.restaurants[state.currentRestaurantId];
+const getRestaurants = state => Object.values(state.restaurants);
+const getFiltersPredicates = state => Object.values(state.filters.predicates);
 
 export const getCurrentRestaurant = createSelector(
   [getCurrentRestaurantById],
   currentRestaurantById => currentRestaurantById
+);
+
+export const getFilteredRestaurants = createSelector(
+  [getRestaurants, getFiltersPredicates],
+  (restaurants, predicateFunctions) =>
+    restaurants.filter(restaurant =>
+      predicateFunctions.every(predicateFunction =>
+        predicateFunction(restaurant)
+      )
+    )
 );
