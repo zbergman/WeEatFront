995ffee0a37db79e352a72b15c52d51dd675a3e3
zohar.bergman <zohar.bergman@wework.com>
2019-09-22 12:14:59 +0300

    Testing filters by predicates

--- src/selectors/RestaurantSelectors.js
+++ src/selectors/RestaurantSelectors.js
@@ -1,9 +1,14 @@
 import { createSelector } from "reselect";
+import { filterByPredicates } from "../utils/Utilities";
 
 const getCurrentRestaurantById = state =>
-  state.restaurantReducer.restaurants[state.restaurantReducer.currentRestaurantId];
-const getRestaurants = state => Object.values(state.restaurantReducer.restaurants);
-const getFiltersPredicates = state => Object.values(state.restaurantReducer.filters.predicates);
+  state.restaurantReducer.restaurants[
+    state.restaurantReducer.currentRestaurantId
+  ];
+const getRestaurants = state =>
+  Object.values(state.restaurantReducer.restaurants);
+const getFiltersPredicates = state =>
+  Object.values(state.restaurantReducer.filters.predicates);
 
 export const getCurrentRestaurant = createSelector(
   [getCurrentRestaurantById],
@@ -13,9 +18,5 @@ export const getCurrentRestaurant = createSelector(
 export const getFilteredRestaurants = createSelector(
   [getRestaurants, getFiltersPredicates],
   (restaurants, predicateFunctions) =>
-    restaurants.filter(restaurant =>
-      predicateFunctions.every(predicateFunction =>
-        predicateFunction(restaurant)
-      )
-    )
+    filterByPredicates(restaurants, predicateFunctions)
 );
